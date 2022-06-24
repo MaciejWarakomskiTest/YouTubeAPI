ed5a2b24e6cc990041d8297feb7dfcde133cc4b5
Blade Studios <maciejwarakomski@gmail.com>
2022-06-24 21:12:15 +0200

    Pomyślne wyświetlenie playlist YouTube zalogowanego użytkownika plus poprawki wizualne

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <title>YouTube API Test</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <script src="https://accounts.google.com/gsi/client" async defer></script>
@@ -12,9 +13,19 @@
 if(empty($_POST['credential']) && empty($_SESSION['access_token']))
     header("Location: login.php");
 
-$test = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&maxResults=25&mine=true&key=AIzaSyAXiFwZ8gH9uE7LDv2hPkroBPzl5R4CGMU&access_token=".$_SESSION['access_token']));
-echo "lista: <pre>";
-print_r($test);
+$result = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&maxResults=25&mine=true&key==AIzaSyAXiFwZ8gH9uE7LDv2hPkroBPzl5R4CGMU&access_token=".$_SESSION['access_token']));
+echo "<div class='title'>PLAYLISTY MOJEGO KANAŁU YOUTUBE</div>";
+
+echo "<div class='lista'>";
+foreach($result->items as $item)
+{
+    echo $item->snippet->title."<br>";
+}
+echo '</div>
+<br>
+<form action="login.php">
+    <input type="submit" value="Przejdź do strony logowania" />
+</form>';
 
 ?>
 
--- login.php
+++ login.php
@@ -3,17 +3,18 @@
 <html>
 <head>
     <title>YouTube API Test</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<a href="https://accounts.google.com/o/oauth2/v2/auth?
+<div class="cen"><a class="zaloguj" href="https://accounts.google.com/o/oauth2/v2/auth?
 response_type=code&
 client_id=885218019365-77rti44e4g0d7us6dcih9vbf3u8p497d.apps.googleusercontent.com&
 redirect_uri=http://localhost/YouTubeAPI/auth.php&
 scope=https%3A//www.googleapis.com/auth/youtube.readonly&
 state=1234567&
 access_type=offline&
-include_granted_scopes=true">ZALOGUJ</a>
+include_granted_scopes=true">ZALOGUJ</a></div>
 
 </body>
 </html>
